c863b91b408169ae291cb6dad6e51e0c276510c5
Randall Gray <randall.gray7260@gmail.com>
2020-04-27 18:26:14 -0500

    Remove() is complete.  Passed all unit tests.  Item at end of internal array is set back to default value on a Remove() call.  Indexer fixed to throw an exception if value is out of range.

--- CustomListGeneric/CustomList.cs
+++ CustomListGeneric/CustomList.cs
@@ -18,8 +18,20 @@ namespace CustomListGeneric
         // indexer
         public T this[int i]
         {
-            get { return items[i]; }
-            set { items[i] = value; }
+            get 
+            { 
+                if (i > 0 && i < count) 
+                    return items[i]; 
+                else
+                    throw new IndexOutOfRangeException($"Index {i} is out of range.");
+            }
+            set 
+            { 
+                if (i > 0 && i < count)
+                   items[i] = value;
+                else
+                    throw new IndexOutOfRangeException($"Index {i} is out of range.");
+            }
         }
 
         // constructor
@@ -34,26 +46,69 @@ namespace CustomListGeneric
         // Adds item to end of list.
         public void Add(T item)
         {
-            if (count == capacity)    // expand capacity of internal array
+            if (count == capacity)
             {
-                T[] tempArray = new T[capacity * 2];
-                for (int i = 0; i < count; i++)
-                {
-                    tempArray[i] = items[i];
-                }
-                items = tempArray;
-                capacity *= 2;
+                ExpandCapacityOfList();
             }
             items[count] = item;
             count++;
+        }
+
+        private void ExpandCapacityOfList()
+        {
+            T[] tempArray = new T[capacity * 2];        // double capacity each time
+            
+            for (int i = 0; i < count; i++)
+            {
+                tempArray[i] = items[i];
+            }
+            items = tempArray;
+            capacity *= 2;
         }
 
         // Removes first occurance of item from the list.
         // Returns true if item was found and removed from the list.
         public bool Remove(T item)
         {
-            return false;
+            int index = FindIndexOfItemInList(item);
+
+            if (index < 0)      // item not found in list
+                return false;
+
+            RemoveItemFromListAtIndex(index);
+
+            return true;
         }
 
+        // Returns -1 for the index if the item is not found in the list.
+        private int FindIndexOfItemInList(T item)
+        {
+            bool found = false;
+            int index = 0;
+
+            // find item in list
+            for (; index < count; index++)
+            {
+                if (items[index].Equals(item))
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                return -1;
+            return index;
+        }
+
+        // Item is removed and all subsequent items are moved up.  Capacity of list is unchanged.
+        private void RemoveItemFromListAtIndex(int index)
+        {
+            for (int i = index; i < count - 1; i++)
+            {
+                items[i] = items[i + 1];
+            }
+            count--;
+            items[count] = default(T);      // "zero" out the item at the end that's no longer part of the list.
+        }
     }
 }
--- CustomListGeneric/Program.cs
+++ CustomListGeneric/Program.cs
@@ -10,6 +10,16 @@ namespace CustomListGeneric
     {
         static void Main(string[] args)
         {
+            CustomList<int> list = new CustomList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Remove(5);
+            list.Remove(1);
+            Console.WriteLine(list[2]);
+            Console.ReadLine();
         }
     }
 }
