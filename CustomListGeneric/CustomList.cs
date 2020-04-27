e2c6c04f34796f07e07ea88fad26813c1b39d1a6
Randall Gray <randall.gray7260@gmail.com>
2020-04-27 16:11:30 -0500

    Add() and Remove() unit tests complete.  Add() complete.  Passes all unit tests.

--- CustomListGeneric/CustomList.cs
+++ CustomListGeneric/CustomList.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace CustomListGeneric
 {
-    class CustomList
+    public class CustomList<T>
     {
+        // Member variables
+        private T[] items;
+        private int count;
+        public int Count { get { return count; } }
+        private int capacity;
+        public int Capacity { get { return capacity; } }
+
+        // indexer
+        public T this[int i]
+        {
+            get { return items[i]; }
+            set { items[i] = value; }
+        }
+
+        // constructor
+        public CustomList()
+        {
+            items = new T[4];
+            count = 0;
+            capacity = 4;
+        }
+
+        // Member methods
+        // Adds item to end of list.
+        public void Add(T item)
+        {
+            if (count == capacity)    // expand capacity of internal array
+            {
+                T[] tempArray = new T[capacity * 2];
+                for (int i = 0; i < count; i++)
+                {
+                    tempArray[i] = items[i];
+                }
+                items = tempArray;
+                capacity *= 2;
+            }
+            items[count] = item;
+            count++;
+        }
+
+        // Removes first occurance of item from the list.
+        // Returns true if item was found and removed from the list.
+        public bool Remove(T item)
+        {
+            return false;
+        }
+
     }
 }
--- UnitTestCustomList/UnitTest1.cs
+++ UnitTestCustomList/UnitTest1.cs
@@ -8,8 +8,229 @@ namespace UnitTestCustomList
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_AddingOneValueToEmptyCustomList_AddedValueGoesToIndexZero()
         {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expectedItem = itemToAdd;
+            int actualItem;
+
+            // Act
+            testList.Add(itemToAdd);
+            actualItem = testList[0];
+
+            //Assert
+            Assert.AreEqual(expectedItem, actualItem);
+        }
+
+        [TestMethod]
+        public void Add_AddingOneValueToEmptyCustomList_CountOfCustomListGoesToOne()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expectedValue = 1;
+            int actualValue;
+
+            // Act
+            testList.Add(itemToAdd);
+            actualValue = testList.Count;
+
+            // Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void Add_AddingOneValueToCustomListWithOneItem_AddedValueGoesToIndexOne()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expectedItem = itemToAdd;
+            int actualItem;
+
+            // Act
+            testList.Add(itemToAdd);
+            testList.Add(itemToAdd);
+            actualItem = testList[1];
+
+            //Assert
+            Assert.AreEqual(expectedItem, actualItem);
+        }
+
+        [TestMethod]
+        public void Add_AddingOneValueToCustomListWithFullCapacityOfItems_CapacityOfCustomListDoubles()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expectedValue = testList.Capacity * 2;
+            int actualValue;
+
+            // Act
+            for (int i = testList.Capacity; i > 0; i--)     // Fill list to capacity.
+            {
+                testList.Add(itemToAdd);
+            }
+            testList.Add(itemToAdd);        // Go over capacity
+            actualValue = testList.Capacity;
+
+            //Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void Add_AddingOneValueToCustomListWithFullCapacityOfItems_CountOfCustomListIncrements()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expectedValue = testList.Capacity + 1;
+            int actualValue;
+
+            // Act
+            for (int i = testList.Capacity; i > 0; i--)     // Fill list to capacity.
+            {
+                testList.Add(itemToAdd);
+            }
+            testList.Add(itemToAdd);        // Go over capacity
+            actualValue = testList.Count;
+
+            //Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void Remove_RemoveItemFromAnEmptyCustomList_ReturnsFalse()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToRemove = 5;
+            bool expectedValue = false;
+            bool actualValue;
+
+            // Act
+            actualValue = testList.Remove(itemToRemove);
+
+            // Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void Remove_RemoveItemNotFoundInNonEmptyCustomList_ReturnsFalse()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int firstItem = 1;
+            int secondItem = 2;
+            int thirdItem = 3;
+            int itemToRemove = 5;
+            bool expectedValue = false;
+            bool actualValue;
+
+            // Act
+            testList.Add(firstItem);
+            testList.Add(secondItem);
+            testList.Add(thirdItem);
+
+            actualValue = testList.Remove(itemToRemove);
+
+            // Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void Remove_RemoveItemFoundAtIndexZeroOfCustomList_IndexOneItemBecomesIndexZeroItem()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int firstItem = 1;
+            int secondItem = 2;
+            int thirdItem = 3;
+            int expectedValue = secondItem;
+            int actualValue;
+
+            // Act
+            testList.Add(firstItem);
+            testList.Add(secondItem);
+            testList.Add(thirdItem);
+
+            testList.Remove(firstItem);
+            actualValue = testList[0];
+
+            // Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void Remove_RemoveItemFoundAtIndexZeroOfCustomList_CountOfCustomListDecrements()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int firstItem = 1;
+            int secondItem = 2;
+            int thirdItem = 3;
+            int expectedValue = 2;
+            int actualValue;
+
+            // Act
+            testList.Add(firstItem);
+            testList.Add(secondItem);
+            testList.Add(thirdItem);
+
+            testList.Remove(firstItem);
+            actualValue = testList.Count;
+
+            // Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void Remove_RemoveItemFoundInCustomListOneItemOverCapacity_CapacityOfCustomListDoesNotChange()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expectedValue = testList.Capacity * 2;
+            int actualValue;
+
+            // Act
+            for (int i = testList.Capacity; i > 0; i--)     // Fill list to capacity.
+            {
+                testList.Add(itemToAdd);
+            }
+            testList.Add(itemToAdd);        // Go over capacity
+            testList.Remove(itemToAdd);     // Go back to initial capacity
+            actualValue = testList.Capacity;
+
+            // Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void Remove_RemoveItemFoundInCustomListMultipleTimes_OnlyFirstInstanceOfItemIsRemoved()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int firstItem = 1;
+            int secondItem = 2;
+            int thirdItem = 1;
+            int fourthItem = 3;
+            int expectedValue = thirdItem;
+            int actualValue;
+
+            // Act
+            testList.Add(firstItem);
+            testList.Add(secondItem);
+            testList.Add(thirdItem);
+            testList.Add(fourthItem);
+
+            testList.Remove(thirdItem);
+            actualValue = testList[1];
+
+            // Assert
+            Assert.AreEqual(expectedValue, actualValue);
         }
     }
 }
